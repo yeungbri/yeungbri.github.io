3a3a373e4a4e4eeb14312080e15e09df42248556
Yeung <C85229@internal.cigna.com>
2017-09-25 15:08:11 -0400

    add homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+	{ path: 'home', component: HomeComponent },
+	{ path: '', redirectTo: '/home', pathMatch:'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+  	RouterModule.forRoot(
+  		appRoutes,
+  		{ enableTracing: true }
+  	),
     BrowserModule
   ],
   providers: [],
